8318fdac9d3d27f12829f44785175ce00eaea3f3
Cristian Samaniego <chris.rubiano42@gmail.com>
2015-10-09 00:04:05 -0700

    comente algunas cosas

--- blog/forms.py
+++ blog/forms.py
@@ -1,8 +1,9 @@
+#Este es el formulario donde se crean los posts sin tener que entrar al panel de administracion
 from django import forms
 from .models import Post
 
 class PostForm(forms.ModelForm):
 
     class Meta:
-        model = Post
-        fields = ('title', 'text',)
+        model = Post 	#manda llamar al modelo post
+        fields = ('title', 'text',)  #manda llamar los atributos de titulo y texto del modelo post
